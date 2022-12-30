ef353d6fea3dca422d2b0ed5d3b7db6b22efd592
Sai Krishna <108942315+saikrishnasnehith@users.noreply.github.com>
2022-12-30 13:29:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,29 @@
-var n1=Math.floor(Math.random()*6)+1;
-var n2=Math.floor(Math.random()*6)+1;
-var n3=Math.floor(Math.random()*6)+1;
-document.querySelector("img").setAttribute("src","images/dice"+n1+".png");
-document.querySelectorAll("img")[1].setAttribute("src","images/dice"+n2+".png");
-document.querySelectorAll("img")[2].setAttribute("src","images/dice"+n3+".png");
-document.querySelector("h1").innerHTML="player1 wins";
-if(n1===n2 && n2===n3)
-document.querySelector("h1").innerHTML="draw";
-if(n1>n2)
-{
-  if(n1>n3)
-  {
-    document.querySelector("h1").innerHTML="player1 wins";
-  }
-  else if(n3>n1)
-  {
-    document.querySelector("h1").innerHTML="player3 wins";
-  }
-  else
-  {
-    document.querySelector("h1").innerHTML="player1,3 wins";
-  }
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
 }
-else if(n2>n1)
-{
-   if(n2>n3)
-   {
-     document.querySelector("h1").innerHTML="player2 wins";
-   }
-   else if(n3>n2)
-   {
-     document.querySelector("h1").innerHTML="player3 wins";
-   }
-   else
-   {
-     document.querySelector("h1").innerHTML="player2,3 wins";
-   }
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 }
-else
-{
-  if(n1>n3)
-  {
-    document.querySelector("h1").innerHTML="player1,2 wins";
-  }
-  else if(n3>n1)
-  {
-    document.querySelector("h1").innerHTML="player3 wins";
-  }
-  else
-  {
-    document.querySelector("h1").innerHTML="draw";
-  }
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
 }
